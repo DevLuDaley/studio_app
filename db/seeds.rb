05e611f232e305d7cfb671d49316f7e3b29e881d
lhsharing <daley.luis@gmail.com>
2019-05-16 15:20:50 -0400

    seed artists to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+artists = Artist.create([{ name: 'Coach' },
+                         { name: 'Biggie' },
+                         { name: 'Nas' },
+                         { name: 'Coldplay' },
+                         { name: 'Prince' },
+                         { name: 'Eve' },
+                         { name: 'Gwen' }])
