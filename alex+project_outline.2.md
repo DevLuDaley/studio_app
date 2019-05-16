0c9ee8df25ba7145015478fb9ee3dcb129564612
lhsharing <daley.luis@gmail.com>
2019-05-16 15:34:42 -0400

    update user_name => name

---
+++ alex+project_outline.2.md
@@ -0,0 +1,43 @@
+LE (Aesthician)
+clients
+products
+
+
+Only use Devise on 1 model.
+radio button for are you an engineer or an artist.
+
+
+
+
+
+
+many to many to many (recording_session) joins all 3
+
+nested route =  studios/id/session/new (associates that session to that studio and itself(engineer), will also need an artist)
+
+form for new session (CRUD for session can be reused) need show/ index for :session 
+
+artist (use a dropdown)
+(need new/show/index)
+
+engineer/user => (name)
+has_many sessions
+has_many artists through sessions
+has_many studios through sessions
+
+studio (name, location)
+has_many sessions
+has_many artists through sessions
+has_many engineers through sessions
+
+
+artist => (name/account) 
+artist has_many sessions
+artist has_many engineers through sessions
+has_many studios through sessions
+
+session => (date/time(maybe string as a placeholder), studio, status, engineer, artist)
+belongs_to artist
+belongs_to engineer
+belongs_to studio
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,22 +25,27 @@ studios = Studio.create([{name: "Lu's Closet", location: 'Harlem, New York'},
                          {name: "Beat Farm", location: 'Harlem, New York'},
                          {name: "Grandma's Basement", location: 'Harlem, New York'}])
 
-engineers = Engineer.create([{email: "lh@lh.com", password: 'password', user_name: "lu"},
-                             {email: "young@guru.com", password: 'password', user_name: "young guru"},
-                             {email: "stush@stush.com", password: 'password', user_name: "stush"},
-                             {email: "dre@dre.com", password: 'password', user_name: "dr. dre"},
-                             {email: "timbo@timbo.com", password: 'password', user_name: "timbaland"},
-                             {email: "rick@rick.com", password: 'password', user_name: "rick ruben"}])
-                             {email: "teddy@teddy.com", password: 'password', user_name: "teddy riley"}])
-                             {email: "gilly@gilly.com", password: 'password', user_name: "gilly"},
+engineers = Engineer.create([{email: "lh@lh.com", password: 'password', name: "lu"},
+                             {email: "young@guru.com", password: 'password', name: "young guru"},
+                             {email: "stush@stush.com", password: 'password', name: "stush"},
+                             {email: "dre@dre.com", password: 'password', name: "dr. dre"},
+                             {email: "timbo@timbo.com", password: 'password', name: "timbaland"},
+                             {email: "rick@rick.com", password: 'password', name: "rick ruben"},
+                             {email: "gilly@gilly.com", password: 'password', name: "gilly"},
+                             {email: "teddy@teddy.com", password: 'password', name: "teddy riley"}])
 
-#([{ user_name: 'Lu'}, {user_name: 'Stush'}, {user_name: 'Podrick'}, {user_name: 'Arya'}, {user_name: 'Teddy'}])
+#([{ name: 'Lu'}, {name: 'Stush'}, {name: 'Podrick'}, {name: 'Arya'}, {name: 'Teddy'}])
 #RecSession.create(appointment_date: '6.1.2019', engineer: Engineer.first, studio: Studio.first, artist: Artist.first )
-RecSession.create(appointment_date: '6.1.2019', engineer: engineers[0], studio: studios[0], artist: artists[0])
-RecSession.create(appointment_date: '6.1.2019', engineer: engineers[1], studio: studios[1], artist: artists[1])
-RecSession.create(appointment_date: '6.2.2019', engineer: engineers[2], studio: studios[2], artist: artists[2])
-RecSession.create(appointment_date: '6.2.2019', engineer: engineers[3], studio: studios[3], artist: artists[3])
-RecSession.create(appointment_date: '6.2.2019', engineer: engineers[4], studio: studios[4], artist: artists[4])
+RecSession.create(appointment_date: '6.3.2019', engineer: engineers[0], studio: studios[0], artist: artists[0])
+RecSession.create(appointment_date: '6.4.2019', engineer: engineers[0], studio: studios[0], artist: artists[0])
+RecSession.create(appointment_date: '6.3.2019', engineer: engineers[1], studio: studios[1], artist: artists[1])
+RecSession.create(appointment_date: '6.4.2019', engineer: engineers[1], studio: studios[1], artist: artists[1])
+RecSession.create(appointment_date: '6.3.2019', engineer: engineers[2], studio: studios[2], artist: artists[2])
+RecSession.create(appointment_date: '6.4.2019', engineer: engineers[2], studio: studios[2], artist: artists[2])
+RecSession.create(appointment_date: '6.3.2019', engineer: engineers[3], studio: studios[3], artist: artists[3])
+RecSession.create(appointment_date: '6.4.2019', engineer: engineers[3], studio: studios[3], artist: artists[3])
+RecSession.create(appointment_date: '6.3.2019', engineer: engineers[4], studio: studios[4], artist: artists[4])
+RecSession.create(appointment_date: '6.4.2019', engineer: engineers[4], studio: studios[4], artist: artists[4])
 #Rec_session.create(date: '6.2.2019', eg: engineers[1])
 #Rec_session.create(date: '6.3.2019', eg: engineers[2])
 #Rec_session.create(date: '6.4.2019', eg: engineers[3])s
