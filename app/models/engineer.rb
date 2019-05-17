655f7c207ee2c9e3b9d780d7cb2d6c6629c44abe
lhsharing <daley.luis@gmail.com>
2019-05-17 11:49:02 -0400

    +self.from_omniauth(auth)

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -2,7 +2,7 @@ class Engineer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
          has_many :rec_sessions #adds methods to my model
          #adds methods to my model
@@ -14,4 +14,15 @@ class Engineer < ApplicationRecord
     #validates :name, presence => true
     #use_this=> validates :name, presence: true, length: { minimum: 2 }
     #use_this=>validates :name, uniqueness: true
+
+def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |engineer|
+    engineer.provider = auth.provider
+    engineer.name = auth.name
+    engineer.uid = auth.uid
+    engineer.email = authi.info.email
+    engineer.password = Devise.friendly_token[0,20]
+
 end
+
+#end
