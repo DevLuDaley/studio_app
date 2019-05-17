57482d3d476586a299ecf8f8880e5e534e8678c8
lhsharing <daley.luis@gmail.com>
2019-05-17 10:14:47 -0400

    +gh & omni connected via devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,6 +261,10 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
+config.omniauth :github, '2d6fc7b2435831b65c23', 'bf56208f0384c7d56437e5bcfc5c60916747c6dd', scope: 'engineer:email'
+
+
+  
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
