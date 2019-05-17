65f6ef1224a048738edab7ee5cb127d16ccc23f0
lhsharing <daley.luis@gmail.com>
2019-05-17 14:08:50 -0400

    +troubleshooting bc :omniauthable raises error

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -1,8 +1,8 @@
 class Engineer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable , :omniauthable
+  #devise :omniauthable
 
          has_many :rec_sessions #adds methods to my model
          #adds methods to my model
@@ -15,14 +15,15 @@ class Engineer < ApplicationRecord
     #use_this=> validates :name, presence: true, length: { minimum: 2 }
     #use_this=>validates :name, uniqueness: true
 
-def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |engineer|
-    engineer.provider = auth.provider
-    engineer.name = auth.name
-    engineer.uid = auth.uid
-    engineer.email = authi.info.email
-    engineer.password = Devise.friendly_token[0,20]
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |engineer|
+      engineer.provider = auth.provider
+      engineer.name = auth.name
+      engineer.uid = auth.uid
+      engineer.email = authi.info.email
+      engineer.password = Devise.friendly_token[0,20]
 
+  end
+
+  end
 end
-
-#end
