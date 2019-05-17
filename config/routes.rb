db148c4eb1577b32a44aa2299fc291893279d655
lhsharing <daley.luis@gmail.com>
2019-05-17 11:48:28 -0400

    changing the scope method inside config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :rec_sessions
-  devise_for :artists
-  devise_for :studios
-  devise_for :engineers
+  #devise_for :rec_sessions
+  #devise_for :artists
+  #devise_for :studios
+  #devise_for :engineers, :controllers => {:registrations => "registrations"}
+  devise_for :engineers, :controllers => { omniauth_callbacks: "callbacks" }
+  devise_scope :engineers do
+    get 'login', to: 'devise/sessions#new'
+  end
+  devise_scope :engineers do
+    get 'signup', to: 'devise/registrations#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+
+
+#root 'engineers#index'
 end
