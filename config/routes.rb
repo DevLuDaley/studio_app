56add901ebfed850648b297a47219dc605b86aa7
lhsharing <daley.luis@gmail.com>
2019-05-17 12:08:54 -0400

    bughunting bc /signup isn't working

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,20 @@ Rails.application.routes.draw do
   #devise_for :artists
   #devise_for :studios
   #devise_for :engineers, :controllers => {:registrations => "registrations"}
+  #devise_for :engineers, :controllers => { :omniauth_callbacks => "callbacks" }
   devise_for :engineers, :controllers => { omniauth_callbacks: "callbacks" }
-  devise_scope :engineers do
+  devise_scope :engineer do
     get 'login', to: 'devise/sessions#new'
   end
-  devise_scope :engineers do
+  devise_scope :engineer do
     get 'signup', to: 'devise/registrations#new'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-
+# devise_scope :user do
+#   get "/sign_up" => "devise/registrations#new"
+# end
 
 #root 'engineers#index'
 end
