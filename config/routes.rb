266200c9c2b4960ac411c7f43c17a63021c70c91
lhsharing <daley.luis@gmail.com>
2019-05-17 14:07:23 -0400

    +devise controllers calbacks

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,17 @@ Rails.application.routes.draw do
   #devise_for :studios
   #devise_for :engineers, :controllers => {:registrations => "registrations"}
   #devise_for :engineers, :controllers => { :omniauth_callbacks => "callbacks" }
-  devise_for :engineers, :controllers => { omniauth_callbacks: "callbacks" }
-  devise_scope :engineer do
-    get 'login', to: 'devise/sessions#new'
-  end
-  devise_scope :engineer do
-    get 'signup', to: 'devise/registrations#new'
-  end
+  devise_for :engineers, :controllers => { omniauth_callbacks: "callbacks", :registrations => "registrations" }
+  # devise_scope :engineer do
+  #   #get 'login', to: 'devise/sessions#new'
+  #   get 'login' => 'devise/sessions#new'
+  # end
+  # devise_scope :engineer do3
+  #   #get 'signup', to: 'devise/registrations#new'
+  #   get 'signup' => 'devise/registrations#new'
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+#root home#index"
 
 # devise_scope :user do
 #   get "/sign_up" => "devise/registrations#new"
